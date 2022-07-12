b2c0c07195a1947c4e97153949065ef4607cd103
fabio2100 <squizzato.fabio@gmail.com>
2022-07-11 22:57:49 -0300

    Angular works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { HardAndSoftComponent } from './hard-and-soft/hard-and-soft.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardAndSoftComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hard-and-soft/hard-and-soft.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-and-soft',
+  templateUrl: './hard-and-soft.component.html',
+  styleUrls: ['./hard-and-soft.component.css']
+})
+export class HardAndSoftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
